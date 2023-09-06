5a2df24826a8bd855c566c78bc7314ad78a67473
Nikolay Sosin <102407703+nmsosin@users.noreply.github.com>
2023-09-06 13:51:42 +0300

    Upd deps & types versions

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { FibonacciPage } from "../fibonacci-page/fibonacci-page";
-import { ListPage } from "../list-page/list-page";
-import { MainPage } from "../main-page/main-page";
-import { QueuePage } from "../queue-page/queue-page";
-import { StringComponent } from "../string/string";
-import { SortingPage } from "../sorting-page/sorting-page";
-import { StackPage } from "../stack-page/stack-page";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { FibonacciPage } from "../../pages/fibonacci-page/fibonacci-page";
+import { ListPage } from "../../pages/list-page/list-page";
+import { MainPage } from "../../pages/main-page/main-page";
+import { QueuePage } from "../../pages/queue-page/queue-page";
+import { StringComponent } from "../../pages/string/string";
+import { SortingPage } from "../../pages/sorting-page/sorting-page";
+import { StackPage } from "../../pages/stack-page/stack-page";
 
 import "./app.css";
 
@@ -14,29 +14,20 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        <Switch>
-          <Route path="/" exact>
-            <MainPage />
-          </Route>
-          <Route path="/recursion">
-            <StringComponent />
-          </Route>
-          <Route path="/fibonacci">
-            <FibonacciPage />
-          </Route>
-          <Route path="/sorting">
-            <SortingPage />
-          </Route>
-          <Route path="/stack">
-            <StackPage />
-          </Route>
-          <Route path="/queue">
-            <QueuePage />
-          </Route>
-          <Route path="/list">
-            <ListPage />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/recursion" element={<StringComponent />} />
+          <Route path="/fibonacci" element={<FibonacciPage />} />
+
+          <Route path="/sorting" element={<SortingPage />} />
+
+          <Route path="/stack" element={<StackPage />} />
+
+          <Route path="/queue" element={<QueuePage />} />
+
+          <Route path="/list" element={<ListPage />} />
+
+        </Routes>
       </BrowserRouter>
     </div>
   );
