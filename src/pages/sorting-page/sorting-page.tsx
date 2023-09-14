247c34734ba4e90e06549faba855affd18a07258
Nikolay Sosin <102407703+nmsosin@users.noreply.github.com>
2023-09-14 17:59:21 +0300

    Add basic sorting page layout

--- src/pages/sorting-page/sorting-page.tsx
+++ src/pages/sorting-page/sorting-page.tsx
@@ -1,10 +1,76 @@
-import React from "react";
-import { SolutionLayout } from "../../components/ui/solution-layout/solution-layout";
+import React, {useState} from "react";
+import {SolutionLayout} from "../../components/ui/solution-layout/solution-layout";
+import sortingPageStyles from "./sorting-page.module.css";
+import {Button} from "../../components/ui/button/button";
+import {RadioInput} from "../../components/ui/radio-input/radio-input";
+import {Direction} from "../../types/direction";
+import {Column} from "../../components/ui/column/column";
 
 export const SortingPage: React.FC = () => {
+  const [numbers, setNumbers] = useState<number[]>([])
+
+  const getRandomNumber = (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  const getRandomNumbers = (min:number, max: number): number[] => {
+    const numbersArray: number[] = [];
+
+    for (let i = 0; i < getRandomNumber(3, 17); i++) {
+      numbersArray.push(getRandomNumber(min, max))
+    }
+
+    return numbersArray;
+  }
+
+  const setRandomNumbers = (): void => {
+    setNumbers(getRandomNumbers(0, 100))
+  }
+
   return (
     <SolutionLayout title="Сортировка массива">
+      <div className={sortingPageStyles.container}>
+        <div className={sortingPageStyles.sortingType}>
+          <RadioInput
+            label={'Выбор'}
+            value={'default'}
+          />
+          <RadioInput
+            label={'Пузырёк'}
+          />
+        </div>
 
+        <div className={sortingPageStyles.sortingOrder}>
+          <Button
+            text={'По возрастанию'}
+            sorting={Direction.Ascending}
+          />
+          <Button
+            text={'По убыванию'}
+            sorting={Direction.Descending}
+          />
+        </div>
+
+        <Button
+          text={'Новый массив'}
+          onClick={setRandomNumbers}
+        />
+      </div>
+
+      <ul className={sortingPageStyles.performanceContainer}>
+        {
+          numbers &&
+          numbers.map((item, index) => {
+            return (
+              <li key={index}>
+                <Column
+                  index={item}
+                />
+              </li>
+            )
+          })
+        }
+      </ul>
     </SolutionLayout>
   );
 };
--- src/services/algorythms.ts
+++ src/services/algorythms.ts
@@ -22,6 +22,10 @@ export const getFibonacciNumber = (index: number): number[] => {
 }
 // array sort
 
+   // selection sort
+
+   // bubble sort
+
 // stack
 
 // queue
