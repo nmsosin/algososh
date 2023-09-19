25c4f5f94558d9ce32fb8027b2e638994264b637
Nikolay Sosin <102407703+nmsosin@users.noreply.github.com>
2023-09-19 22:55:34 +0300

    Add string iterative reverse animation

--- src/pages/string/string.tsx
+++ src/pages/string/string.tsx
@@ -1,27 +1,78 @@
-import React, {FormEvent, useState} from "react";
+import React, {FormEvent, useEffect, useState} from "react";
 import { SolutionLayout } from "../../components/ui/solution-layout/solution-layout";
 import {Button} from "../../components/ui/button/button";
 import {Input} from "../../components/ui/input/input";
 import StringPageStyles from './string.module.css'
 import {Circle} from "../../components/ui/circle/circle";
-import {reverseString} from "../../services/algorythms";
+import {ElementStates} from "../../types/element-states";
+import {DELAY_L} from "../../services/constants";
+
+type TLetter = {
+  value?: string;
+  state?: ElementStates;
+}
 
 export const StringComponent: React.FC = () => {
-  const [letters, setLetters] = useState<string[]>([])
   const [inputValue, setInputValue] = useState<string>('');
-
-  const handleButtonClick = (evt: FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
-    console.log(inputValue)
-    const lettersReversed = reverseString(letters)
-    setLetters(inputValue.split(''))
-    reverseString(letters);
-    console.log('letters', letters)
-  }
+  const [circles, setCircles] = useState<TLetter[]>([]);
+  const [buttonDisabled, setButtonDisabled] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleInputChange = (evt: FormEvent<HTMLInputElement>) => {
     setInputValue(evt.currentTarget.value.trim())
-    console.log('inputValue', inputValue)
+  }
+
+  const waitForDelay = (delayTime: number) => new Promise((resolve) => {
+    setTimeout(resolve, delayTime)
+  });
+
+  const replaceCircles = async (inputValue: string) => {
+    const letters = inputValue.split('');
+
+    let circlesArray: any[] = [];
+    for (let i = 0; i < letters.length; i++) {
+      circlesArray.push({
+        value: letters[i],
+        state: ElementStates.Default
+      })
+    }
+
+    setCircles(circlesArray);
+
+    let start = 0;
+    let end = circlesArray.length - 1;
+
+    while (start <= end) {
+      let tempStr = '';
+
+      await waitForDelay(DELAY_L)
+      circlesArray[start].state = ElementStates.Changing;
+      circlesArray[end].state = ElementStates.Changing;
+      setCircles([...circlesArray]);
+
+      await waitForDelay(DELAY_L)
+      tempStr = circlesArray[start].value;
+      circlesArray[start].value = circlesArray[end].value;
+      circlesArray[end].value = tempStr;
+
+      circlesArray[start].state = ElementStates.Modified;
+      circlesArray[end].state = ElementStates.Modified;
+
+      start++;
+      end--;
+      setCircles([...circlesArray]);
+    }
+
+    setButtonDisabled(false);
+    setIsLoading(false);
+  }
+
+  const handleButtonClick = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    setButtonDisabled(true);
+    setIsLoading(true);
+    replaceCircles(inputValue);
+
   }
 
   return (
@@ -37,18 +88,19 @@ export const StringComponent: React.FC = () => {
           <Button
             text={'Развернуть'}
             type={'submit'}
+            disabled={buttonDisabled}
+            isLoader={isLoading}
           />
         </div>
 
         <ul className={StringPageStyles.performanceContainer}>
-          { letters.length > 0 &&
-            letters.map((item, index) => {
+          { circles.length > 0 &&
+            circles.map((item, index) => {
               return (
                 <li key={index} className={StringPageStyles.circle}>
                   <Circle
-                    letter={item}
-                    // index={index}
-
+                    letter={item.value}
+                    state={item.state}
                   />
                 </li>
               )
---
+++ src/services/constants.ts
@@ -0,0 +1,2 @@
+export const DELAY_L = 1000;
+export const DELAY_S = 500;
