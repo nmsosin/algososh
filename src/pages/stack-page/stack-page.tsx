f695447f9a091a007b7fe33e6f7c26db9d1fb9a8
Nikolay Sosin <102407703+nmsosin@users.noreply.github.com>
2023-09-24 15:52:41 +0300

    Add stack class & stack add & delete iterative animation

--- src/pages/stack-page/stack-page.tsx
+++ src/pages/stack-page/stack-page.tsx
@@ -3,26 +3,106 @@ import { SolutionLayout } from "../../components/ui/solution-layout/solution-lay
 import {Button} from "../../components/ui/button/button";
 import {Input} from "../../components/ui/input/input";
 import stackPageStyles from "./stack-page.module.css";
+import {Stack} from "../../services/stack";
+import {Circle} from "../../components/ui/circle/circle";
+import {waitForDelay} from "../../services/utils";
+import {SHORT_DELAY_IN_MS} from "../../constants/delays";
+import {ElementStates} from "../../types/element-states";
 
+const stack = new Stack();
+
+type TStackValue = {
+  value: string;
+  state: ElementStates;
+  head?: boolean;
+}
 export const StackPage: React.FC = () => {
-  const [inputValue, setInputValue] = useState<number | null>(null);
-  const [stackValues, setStackValues] = useState<number[]>([]);
+  const [inputValue, setInputValue] = useState<string >('');
+  const [stackValues, setStackValues] = useState<TStackValue[]>([]);
+  const [buttonDisabled, setButtonDisabled] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleInputChange = (evt: FormEvent<HTMLInputElement>) => {
-    setInputValue(evt.currentTarget.valueAsNumber)
-    console.log('inputValue', inputValue)
+    setInputValue(evt.currentTarget.value)
   }
 
-  const handleAddButton = () => {
+  const handleAddButton = async (item: string) => {
+    if (item && item !== '') {
+      setButtonDisabled(true);
+      setIsLoading(true);
+      stack.push(item);
+      const stackCircles = stack.getStackValues().map((item) => {
+        return {
+          value: item,
+          state: ElementStates.Default,
+          head: false
+        }
+      })
+      stackCircles[stackCircles.length - 1] = {
+        ...stackCircles[stackCircles.length - 1],
+        state: ElementStates.Changing,
+        head: true
+      }
+      setStackValues(stackCircles as TStackValue[])
 
+      await waitForDelay(SHORT_DELAY_IN_MS);
+      const defaultCircles = stackCircles.map((item) => {
+        return {
+          ...item,
+            state: ElementStates.Default
+        }
+      })
+      setStackValues(defaultCircles as TStackValue[])
+    }
+    setInputValue('');
+    setButtonDisabled(false);
+    setIsLoading(false);
   }
 
-  const handleDeleteButton = () => {
-    setStackValues([])
+  const handleDeleteButton = async () => {
+    if (stack) {
+      setButtonDisabled(true);
+      setIsLoading(true);
+    }
+
+    const stackCircles = stack.getStackValues().map((item) => {
+      return {
+        value: item,
+        state: ElementStates.Default,
+        head: false
+      }
+    })
+    stackCircles[stackCircles.length - 1] = {
+      ...stackCircles[stackCircles.length - 1],
+      state: ElementStates.Changing,
+      head: true
+    }
+    setStackValues(stackCircles as TStackValue[])
+
+    await waitForDelay(SHORT_DELAY_IN_MS);
+    stack.pop();
+
+    const defaultCircles = stack.getStackValues().map((item) => {
+      return {
+        value: item,
+        state: ElementStates.Default,
+        head: false
+      }
+    })
+
+    defaultCircles[defaultCircles.length - 1] = {
+      ...defaultCircles[defaultCircles.length - 1],
+      head: true
+    }
+    setStackValues(defaultCircles as TStackValue[])
+
+    setButtonDisabled(false);
+    setIsLoading(false);
   }
 
     const handleClearButton = () => {
-
+    stack.clear();
+    setStackValues([])
   }
 
   return (
@@ -33,24 +113,45 @@ export const StackPage: React.FC = () => {
             type={"text"}
             maxLength={4}
             isLimitText={true}
+            value={inputValue}
             onChange={handleInputChange}
           />
           <Button
             text={"Добавить"}
-            onClick={handleAddButton}
+            onClick={() => handleAddButton(inputValue)}
+            disabled={buttonDisabled}
+            isLoader={isLoading}
           />
           <Button
             text={"Удалить"}
-            onClick={handleDeleteButton}
+            onClick={() => handleDeleteButton()}
+            disabled={buttonDisabled}
+            isLoader={isLoading}
           />
         </div>
         <Button
           text={"Очистить"}
-          onClick={handleClearButton}
+          onClick={() => handleClearButton()}
+          disabled={buttonDisabled}
+          isLoader={isLoading}
         />
       </form>
-      <ul>
-
+      <ul className={stackPageStyles.performanceContainer}>
+        {
+          stackValues &&
+          stackValues.map((item,index) => {
+            return (
+              <li className={stackPageStyles.circle} key={index}>
+                <Circle
+                  letter={item.value}
+                  state={item.state}
+                  index={index}
+                  head={item.head ? 'top' : ''}
+                />
+              </li>
+            )
+          })
+        }
       </ul>
 
     </SolutionLayout>
